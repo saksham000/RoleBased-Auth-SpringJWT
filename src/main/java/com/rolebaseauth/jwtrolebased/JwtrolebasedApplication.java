9d339ded70211f252dc6fdffb2c18952108a6458
Saksham Sharma <sakshamshankyan7@gmail.com>
2024-09-16 00:07:22 +0530

    initial commit

---
+++ src/main/java/com/rolebaseauth/jwtrolebased/JwtrolebasedApplication.java
@@ -0,0 +1,13 @@
+package com.rolebaseauth.jwtrolebased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtrolebasedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtrolebasedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rolebaseauth/jwtrolebased/JwtrolebasedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolebaseauth.jwtrolebased;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtrolebasedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
